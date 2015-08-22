d5ef709ffc2116b428b875efab9f045e3af55280
Luthfi Hariz <luthfihariz@gmail.com>
2015-08-23 06:50:17 +0700

    Hapi logo

--- resources/views/map.php
+++ resources/views/map.php
@@ -3,51 +3,22 @@
   <head>
     <meta name="viewport" content="initial-scale=1.0, user-scalable=no">
     <meta charset="utf-8">
-    <title>Fusion Tables layers</title>
+    <title>Hapi - Harga Sapi</title>
     <script type="text/javascript" src="https://www.google.com/jsapi"></script>
     <script type="text/javascript" src="js/jquery-min.js"></script>
     <script type="text/javascript"
-        src="http://maps.google.com/maps/api/js?sensor=false&libraries=places"></script>
+        src="http://maps.google.com/maps/api/js?sensor=false&libraries=places,visualization"></script>
     <!-- Latest compiled and minified CSS -->
+    <link rel="icon" type="img/ico" href="img/ic_cow.png">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
     <link rel="stylesheet" type="text/css" href="css/hapi.css">
-  </head>
-  <body>
-    <div id="panel" class="col-md-12">
-        <!-- <img src="https://placehold.it/80x80" alt="" />
-        <input type="search" class="form-control" placeholder="Cari kecamatan atau pasar" autocomplete="off"> -->
-        <div class="form-group">
-            <div class="col-md-10 col-md-offset-1">
-                <div class="col-xs-5 col-md-7 padding-margin-o">
-                    <input type="search" id="pac-input" class="form-control" placeholder="Cari kecamatan atau pasar" autocomplete="off">
-                </div>
-                <div class="col-md-3">
-                    <button class="btn btn-success" id="report" type="button" name="button">Lapor!</button>
-                </div>
-            </div>
-        </div>
-    </div>
-    <div id="filter">
-        <div class="inner-filter">
-            <div class="col-md-5">
-                <h4>Sumber Data</h4>
-                <input type="radio" name="data" class="regular-bold" value='gov'> Data Pemerintah<br/>
-                <input type="radio" name="data" class="regular-bold" value='comm' checked="checked"> Data Masyarakat (Crowdsourcing)
-            </div>
-            <div class="col-md-3">
-                <h4>Harga Tertinggi Global</h4>
-                <span class="global-highest">Rp117.000 </span><span class="increment">meningkat 1%</span>
-            </div>
-            <div class="col=md-3">
-                <h4>Harga Terendah Global</h4>
-                <span class="global-lowest">Rp109.000</span>
-            </div>
-        </div>
-    </div>
-    <div id="map"></div>
+
     <script type="text/javascript">
+        var service;
+        var infoWindow;
+        var map;
         function initialize() {
-            var map = new google.maps.Map(document.getElementById('map'), {
+            map = new google.maps.Map(document.getElementById('map'), {
                 center: new google.maps.LatLng(-6.182310, 106.828723),
                 zoom: 10,
                 styles:mapStyle
@@ -76,6 +47,8 @@
                 html += "<br/><br/>"
                 html += "<button class='btn btn-success' id='detail' type='button' name='button'>Selengkapnya</button>"
                 e.infoWindowHtml = html;
+
+                console.log(e.row['geometry'].value);
             });
 
             $(document).ready(function() {
@@ -88,7 +61,83 @@
                     layer.setMap(map);
                 });
             });
+
+            infoWindow = new google.maps.InfoWindow();
+            service = new google.maps.places.PlacesService(map);
         }
+
+        function performPlacesSearch(bounds){
+            var request = {
+                location: location,
+                radius: 500,
+                types: ['grocery_or_supermarket']
+              };
+              service.radarSearch(request, callback);
+        }
+
+        function callback(results, status) {
+            console.log("callback");
+          if (status !== google.maps.places.PlacesServiceStatus.OK) {
+            console.error(status);
+            return;
+          }
+          for (var i = 0, result; result = results[i]; i++) {
+            addMarker(result);
+          }
+        }
+
+        function addMarker(place) {
+          var marker = new google.maps.Marker({
+            map: map,
+            position: place.geometry.location,
+            icon: {
+              url: 'http://maps.gstatic.com/mapfiles/circle.png',
+              anchor: new google.maps.Point(10, 10),
+              scaledSize: new google.maps.Size(10, 17)
+            }
+          });
+
+          google.maps.event.addListener(marker, 'click', function() {
+            service.getDetails(place, function(result, status) {
+              if (status !== google.maps.places.PlacesServiceStatus.OK) {
+                console.error(status);
+                return;
+              }
+              infoWindow.setContent(result.name);
+              infoWindow.open(map, marker);
+            });
+          });
+        }
+
+        // var initialLocation;
+        // function centerMapToCurrentLocation(map){
+        //     if(navigator.geolocation) {
+        //         browserSupportFlag = true;
+        //         navigator.geolocation.getCurrentPosition(function(position) {
+        //           initialLocation = new google.maps.LatLng(position.coords.latitude,position.coords.longitude);
+        //           map.setCenter(initialLocation);
+        //         }, function() {
+        //           handleNoGeolocation(browserSupportFlag);
+        //         });
+        //       }
+        //       // Browser doesn't support Geolocation
+        //       else {
+        //         browserSupportFlag = false;
+        //         handleNoGeolocation(browserSupportFlag);
+        //       }
+        // }
+        //
+        // function handleNoGeolocation(errorFlag) {
+        //    if (errorFlag == true) {
+        //      alert("Geolocation service failed.");
+        //      initialLocation = newyork;
+        //    } else {
+        //      alert("Your browser doesn't support geolocation. We've placed you in Siberia.");
+        //      initialLocation = siberia;
+        //    }
+        //    map.setCenter(initialLocation);
+        //    map.setZoom(17);
+        //  }
 
         function updateLayerQuery(layer, data){
             var where = "data_source > "+data;
@@ -100,8 +149,8 @@
                 styles: [{
                     where: "'price_fraction' <= 0.2",
                     polygonOptions: {
-                            fillColor: "#3bb9c8",
-                            fillOpacity: 0.6,
+                            fillColor: "#ED212E",
+                            fillOpacity: 0.2,
                             strokeColor: "#FFFFFF",
                             strokeWeight: 1
                         }
@@ -109,8 +158,8 @@
                     {
                     where: "'price_fraction' > 0.2 AND 'price_fraction' <= 0.4",
                     polygonOptions: {
-                            fillColor: "#3ca0a0",
-                            fillOpacity: 0.6,
+                            fillColor: "#ED212E",
+                            fillOpacity: 0.3,
                             strokeColor: "#FFFFFF",
                             strokeWeight: 1
                         }
@@ -118,8 +167,8 @@
                     {
                     where: "'price_fraction' > 0.4 AND 'price_fraction' <= 0.6",
                     polygonOptions: {
-                            fillColor: "#179647",
-                            fillOpacity: 0.6,
+                            fillColor: "#ED212E",
+                            fillOpacity: 0.5,
                             strokeColor: "#FFFFFF",
                             strokeWeight: 1
                         }
@@ -127,8 +176,8 @@
                     {
                     where: "'price_fraction' > 0.6 AND 'price_fraction' <= 0.8",
                     polygonOptions: {
-                            fillColor: "#ffa300",
-                            fillOpacity: 0.6,
+                            fillColor: "#ED212E",
+                            fillOpacity: 0.7,
                             strokeColor: "#FFFFFF",
                             strokeWeight: 1
                         }
@@ -137,7 +186,7 @@
                     where: "'price_fraction' > 0.8",
                     polygonOptions: {
                             fillColor: "#ED212E",
-                            fillOpacity: 0.6,
+                            fillOpacity: 0.9,
                             strokeColor: "#FFFFFF",
                             strokeWeight: 1
                         }
@@ -212,5 +261,71 @@
         }];
         google.maps.event.addDomListener(window, 'load', initialize);
     </script>
+  </head>
+  <body>
+    <div id="panel" class="col-md-12">
+
+        <!-- <img src="https://placehold.it/80x80" alt="" />
+        <input type="search" class="form-control" placeholder="Cari kecamatan atau pasar" autocomplete="off"> -->
+        <div class="form-group">
+            <div class="col-md-1 margin-100">
+                <img src="img/ic_logo.png" alt="" />
+            </div>
+            <div class="col-md-10 col-md-offset-1" id="search_container">
+                <div class="col-xs-5 col-md-7 padding-margin-o">
+                    <input type="search" id="pac-input" class="form-control" placeholder="Cari kecamatan atau pasar" autocomplete="off">
+                </div>
+                <div class="col-md-3">
+                    <button class="btn btn-success" id="report" type="button" name="button">Lapor!</button>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div id="filter">
+        <div class="inner-filter">
+            <div class="col-md-5">
+                <h4>Sumber Data</h4>
+                <input type="radio" name="data" class="regular-bold" value='gov'> Data Pemerintah<br/>
+                <input type="radio" name="data" class="regular-bold" value='comm' checked="checked"> Data Masyarakat (Crowdsourcing)
+            </div>
+            <div class="col-md-3">
+                <h4>Harga Tertinggi Global</h4>
+                <span class="global-highest">Rp117.000 </span><span class="increment">meningkat 1%</span>
+            </div>
+            <div class="col=md-3">
+                <h4>Harga Terendah Global</h4>
+                <span class="global-lowest">Rp109.000</span>
+            </div>
+        </div>
+    </div>
+    <div id="report-form" class="hide">
+        <div class="inner-filter">
+            <div class="col-md-12">
+                <div class="col-md-4">
+                    <select class="form-control">
+                      <option>Daging Has</option>
+                      <option>Daging Has</option>
+                      <option>Daging Has</option>
+                      <option>Daging Has</option>
+                      <option>Daging Has</option>
+                    </select>
+                </div>
+                <div class="col-md-4">
+                    <input type="text" class="form-control" placeholder="Harga Komoditas" autocomplete="off">
+                </div>
+                <div class="col-md-4">
+                    <button class="btn btn-success" id="save-report" type="button" name="button">Simpan!</button>
+                </div>
+            </div>
+            <div class="col-md-12">
+                <p>
+                    Dengan melapor, Anda membantu memberantas mafia daging sapi yang membuat harga daging mahal!
+                </p>
+            </div>
+        </div>
+    </div>
+
+
+    <div id="map"></div>
   </body>
 </html>
